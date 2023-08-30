78005456a9efb472e64d9130786732e72b1de7c3
Bunchana Kallayanapreecha <bird_sc@hotmail.co.th>
2023-08-30 22:55:07 +0700

    Complete navbar and footer

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,8 +4,10 @@ import Image from "next/image"
 export default function Navbar(){
     return (
         <nav>
-            <div>
-                <Image src={"/logo.png"} width={50} height={50} alt="logo"/>
+            <div className="logo">
+                <Link href={"/"}>
+                    <Image src={"/logo.png"} width={50} height={50} alt="logo"/>
+                </Link>
             </div>
             <Link href={"/"}>หน้าแรก</Link>
             <Link href={"/about"}>เกี่ยวกับ</Link>
