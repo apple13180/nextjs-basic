1d6f0840bffcfbce0397ef08c51e19a9aef16832
Bunchana Kallayanapreecha <bird_sc@hotmail.co.th>
2023-09-02 11:48:56 +0700

    complete detail

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,7 +1,50 @@
-export default function ProductDetail(){
+import Head from "next/head"
+import Image from "next/image"
+import styles from "@/styles/Detail.module.css"
+
+export async function getStaticPaths(){
+    const res = await fetch("https://dummyjson.com/products?limit=12")
+    const data = await res.json()
+    const paths = data.products.map((item)=>{
+        return {
+            params:{id:String(item.id)}
+        }
+    })
+    return{
+        paths,
+        fallback: false
+    }
+}
+export async function getStaticProps({params}){
+    const id = params.id
+    const res = await fetch("https://dummyjson.com/products/"+id)
+    const data = await res.json()
+    return{
+        props: {product:data}
+    }
+}
+
+
+export default function ProductDetail({product}){
+
     return (
         <>
-            <h1>รหัสสินค้า</h1>
+            <Head>
+                <title>{product.title}</title>
+            </Head>
+            <div className={styles.container}>
+                <div>
+                    <Image src={product.thumbnail} width={300} height={300} alt={product.title} />
+                </div>
+                <div className={styles.detail}>
+                    <h1>ชื่อสินค้า : {product.title}</h1>
+                    <h2>ราคา : ${product.price}</h2>
+                    <h2>หมวดหมู่ : {product.category}</h2>
+                    <h3>แบรนด์ : {product.brand}</h3>
+                    <h4>ข้อมูลพื้นฐาน : {product.description}</h4>
+                    <h4>เรทติ้ง : {product.rating}</h4>
+                </div>
+            </div>
         </>
     )
 }
