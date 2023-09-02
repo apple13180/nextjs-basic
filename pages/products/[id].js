8153881d8485ea7192adb9a06bfe98fa793639d4
Bunchana Kallayanapreecha <bird_sc@hotmail.co.th>
2023-09-02 10:48:11 +0700

    connect API add [id] dinamic page

---
+++ pages/products/[id].js
@@ -0,0 +1,7 @@
+export default function ProductDetail(){
+    return (
+        <>
+            <h1>รหัสสินค้า</h1>
+        </>
+    )
+}
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,13 +1,34 @@
 import Head from "next/head"
+import Image from "next/image"
+import style from "@/styles/Product.module.css"
+import Link from "next/link"
 
-export default function Index(){
+export async function getStaticProps(){
+    const res = await fetch("https://dummyjson.com/products?limit=12")
+    const data = await res.json()
+    return{
+        props: {products:data.products}
+    }
+}
+
+
+export default function Index({products}){
     return (
         <>
             <Head>
             <title> สินค้าทั้งหมด | Bird Bunchana</title>
             <meta name="keyword" content="birdbunchana,ร้านค้า,ขายเสื้อผ้า"/>
             </Head>
-            <h1>สินค้าทั้งหมด</h1>
+            <div className={style.container}>
+                {products.map(item=>(
+                    <div key={item.id}>
+                        <Link href={'/products/'+item.id}>
+                            <h2 className={style.title}>{item.title}</h2>
+                            <Image src={item.thumbnail} width={300} height={300} alt={item.title}/>
+                        </Link>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
